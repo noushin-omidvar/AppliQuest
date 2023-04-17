791e44a9495cdb3b289c147d15f2fab642c530e8
Noushin Omidvar <noush.omidvar@gmail.com>
2023-04-17 15:36:45 -0600

    set up APIs

--- app.py
+++ app.py
@@ -31,20 +31,19 @@ connect_db(app)
 # --------------------
 
 
+# Handling user data
+# --------------------------------
 @app.route('/api/users')
 def list_users():
+    """Get all users"""
+
     all_users = [u.as_dict() for u in User.query.all()]
     return jsonify(users=all_users)
 
 
-@app.route('/api/users/<user_id>')
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify(user=user.to_dict())
-
-
-@app.route('/api/users/<user_id>', methods=['POST'])
-def create_user(user_id):
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    """Create a new user"""
 
     new_user = User(first_name=request.json['first_name'],
                     last_name=request.json['last_name'],
@@ -59,10 +58,20 @@ def create_user(user_id):
     return jsonify(new_user=new_user.to_dict())
 
 
+@app.route('/api/users/<user_id>')
+def get_user(user_id):
+    """get user by user_id"""
+
+    user = User.query.get_or_404(user_id)
+    return jsonify(user=user.to_dict())
+
+
 @app.route('/api/users/<user_id>', methods=['PATCH'])
 def update_user(user_id):
+    """Update the user"""
+
     # Retrieve the user from the database
-    user = User.query.get(user_id)
+    user = User.query.get_404(user_id)
 
     if user is None:
         # Return a 404 error if the user is not found
@@ -75,28 +84,125 @@ def update_user(user_id):
     return jsonify(user.to_dict())
 
 
+@app.route('/api/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """DELETE the user"""
+
+    # Retrieve the user from the database
+    user = User.query.get_404(user_id)
+
+    if user is None:
+        # Return a 404 error if the user is not found
+        return jsonify({'error': 'User not found'}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    # Return the updated user data
+    return jsonify(message="User deleted")
+
+
+# Handling the jobs data
+# ------------------------------
+
+@app.route('/api/users/<user_id>/jobs', methods=['POST'])
+def create_job():
+    """Create a new job"""
+
+    new_job = Job(user_id=request.json['user_id'],
+                  company_id=request.json['company_id'],
+                  job_title=request.json['job_title'],
+                  post_url=request.json['post_url'],
+                  application_date=request.json['application_date'],
+                  status=request.json['status'],
+                  notes=request.json['notes'],
+                  job_location=request.json['job_location'],
+                  job_description=request.json['job_description'],
+                  )
+
+    db.session.add(new_job)
+    db.session.commit()
+    return jsonify(new_job=new_job.to_dict())
+
+
+@app.route('/api/users/<user_id>/jobs/<job_id>')
+def get_job(user_id, job_id):
+    """get job by job_id"""
+
+    job = Job.query.get_or_404(job_id)
+    return jsonify(job=job.to_dict())
+
+
 @app.route('/api/users/<user_id>/jobs/<job_id>', methods=['PATCH'])
 def update_job(user_id, job_id):
-    # Retrieve the job from the database
+    """Update the job """
+
     job = Job.query.get(job_id)
+
+    if job is None:
+        return jsonify({'error': 'Job not found'}), 404
+
+    db.session.query(Job).filter(
+        id=job_id, user_id=user_id).update(request.json)
+    db.session.commit()
+
+    return jsonify(job.to_dict())
+
+
+@app.route('/api/users/<user_id>/jobs/<job_id>', methods=['DELETE'])
+def delete_job(job_id):
+    """DELETE the job"""
+
+    # Retrieve the user from the database
+    job = Job.query.get_404(job_id)
 
     if job is None:
         # Return a 404 error if the job is not found
         return jsonify({'error': 'Job not found'}), 404
 
-    db.session.query(Job).filter(id=job_id).update(request.json)
+    db.session.delete(job)
     db.session.commit()
 
     # Return the updated job data
-    return jsonify(job.to_dict())
+    return jsonify(message="job deleted")
+
+
+# Handle contacts data
+# -----------------------
+
+@app.route('/api/users/<user_id>/contacts', methods=['POST'])
+def create_contact():
+    """Create a new contact"""
+
+    new_contact = Contact(user_id=request.json['user_id'],
+                          company_id=request.json['company_id'],
+                          first_name=request.json['first_name'],
+                          last_name=request.json['last_name'],
+                          email=request.json['email'],
+                          phone=request.json['phone'],
+                          notes=request.json['notes'])
+
+    db.session.add(new_contact)
+    db.session.commit()
+    return jsonify(new_contact=new_contact.to_dict())
+
+
+@app.route('/api/users/<user_id>/contacts/<contact_id>')
+def get_contact(user_id, contact_id):
+    """get contact by contact_id"""
+
+    contact = Contact.query.get_or_404(contact_id)
+    return jsonify(contact=contact.to_dict())
 
 
 @app.route('/api/users/<user_id>/contacts/<contact_id>', methods=['PATCH'])
-def update_contact(user_id, contact_id)
-  # Retrieve the contact from the database
-  contact = Contact.query.get(contact_id)
+def update_contact(user_id, contact_id):
+    """ Update contact"""
 
-   if contact is None:
+    # Retrieve the contact from the database
+    contact = Contact.query.get(contact_id)
+
+    if contact is None:
         # Return a 404 error if the contact is not found
         return jsonify({'error': 'Contact not found'}), 404
 
@@ -105,6 +211,204 @@ def update_contact(user_id, contact_id)
 
     # Return the updated contact data
     return jsonify(contact.to_dict())
+
+
+@app.route('/api/users/<user_id>/contacts/<contact_id>', methods=['DELETE'])
+def delete_contact(contact_id):
+    """DELETE the contact"""
+
+    # Retrieve the user from the database
+    contact = Contact.query.get_404(contact_id)
+
+    if contact is None:
+        # Return a 404 error if the contact is not found
+        return jsonify({'error': 'Contact not found'}), 404
+
+    db.session.delete(contact)
+    db.session.commit()
+
+    # Return the updated contact data
+    return jsonify(message="Contact deleted")
+
+
+# Handling documents data
+# -------------------------
+
+@app.route('/api/users/<user_id>/documents', methods=['POST'])
+def create_document():
+    """Create a new document"""
+
+    new_document = Document(user_id=request.json['user_id'],
+                            job_id=request.json['job_id'],
+                            title=request.json['title'],
+                            category=request.json['category'],
+                            file_url=request.json['file_url'])
+
+    db.session.add(new_document)
+    db.session.commit()
+    return jsonify(new_document=new_document.to_dict())
+
+
+@app.route('/api/users/<user_id>/documents/<document_id>')
+def get_document(user_id, document_id):
+    """get document by document_id"""
+
+    document = Document.query.get_or_404(document_id)
+    return jsonify(document=document.to_dict())
+
+
+@app.route('/api/users/<user_id>/documents/<document_id>', methods=['PATCH'])
+def update_document(user_id, document_id):
+    # Retrieve the document from the database
+    document = Document.query.get(document_id)
+
+    if document is None:
+        # Return a 404 error if the document is not found
+        return jsonify({'error': 'Document not found'}), 404
+
+    db.session.query(Document).filter(id=document_id).update(request.json)
+    db.session.commit()
+
+    # Return the updated document data
+    return jsonify(document.to_dict())
+
+
+@app.route('/api/users/<user_id>/documents/<document_id>', methods=['DELETE'])
+def delete_document(document_id):
+    """DELETE the contact"""
+
+    # Retrieve the document from the database
+    document = Document.query.get_404(document_id)
+
+    if document is None:
+        # Return a 404 error if the document is not found
+        return jsonify({'error': 'Document not found'}), 404
+
+    db.session.delete(document)
+    db.session.commit()
+
+    # Return the updated document data
+    return jsonify(message="document deleted")
+
+
+# Handling task data
+# --------------------
+
+@app.route('/api/users/<user_id>/tasks', methods=['POST'])
+def create_task():
+    """Create a new document"""
+
+    new_task = Task(user_id=request.json['user_id'],
+                    job_id=request.json['job_id'],
+                    task=request.json['task'],
+                    created_at=request.json['created_at'],
+                    due_date=request.json['due_date'],
+                    notes=request.json['notes'])
+
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify(new_task=new_task.to_dict())
+
+
+@app.route('/api/users/<user_id>/tasks/<task_id>')
+def get_task(user_id, task_id):
+    """get task by task_id"""
+
+    task = Task.query.get_or_404(task_id)
+    return jsonify(task=task.to_dict())
+
+
+@app.route('/api/users/<user_id>/tasks/<task_id>', methods=['PATCH'])
+def update_task(user_id, task_id):
+    # Retrieve the task from the database
+    task = Task.query.get(task_id)
+
+    if task is None:
+        # Return a 404 error if the task is not found
+        return jsonify({'error': 'Task not found'}), 404
+
+    db.session.query(Task).filter(id=task_id).update(request.json)
+    db.session.commit()
+
+    # Return the updated task data
+    return jsonify(task.to_dict())
+
+
+@app.route('/api/users/<user_id>/tasks/<task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    """DELETE the contact"""
+
+    # Retrieve the document from the database
+    task = Task.query.get_404(task_id)
+
+    if task is None:
+        # Return a 404 error if the document is not found
+        return jsonify({'error': 'task not found'}), 404
+
+    db.session.delete(task)
+    db.session.commit()
+
+    # Return the updated document data
+    return jsonify(message="task deleted")
+
+
+# Handle the companies data
+# --------------------------
+
+@app.route('/api/companiess/', methods=['POST'])
+def create_company():
+    """Create a new company"""
+
+    new_company = Company(company_name=request.json['company_name'],
+                          company_location=request.json['company_location'],
+                          company_url=request.json['company_url'],
+                          company_about=request.json['company_about'])
+
+    db.session.add(new_company)
+    db.session.commit()
+    return jsonify(new_task=new_company.to_dict())
+
+
+@app.route('/api/companies/<company_id>')
+def get_task(user_id, company_id):
+    """get company by company_id"""
+
+    company = Company.query.get_or_404(company_id)
+    return jsonify(company=company.to_dict())
+
+
+@app.route('/api/companies/<company_id>', methods=['PATCH'])
+def update_company(user_id, company_id):
+    # Retrieve the company from the database
+    company = Company.query.get(company_id)
+
+    if company is None:
+        # Return a 404 error if the company is not found
+        return jsonify({'error': 'Company not found'}), 404
+
+    db.session.query(Company).filter(id=company_id).update(request.json)
+    db.session.commit()
+
+    # Return the updated company data
+    return jsonify(company.to_dict())
+
+
+@app.route('/api/companies/<company_id>', methods=['DELETE'])
+def delete_company(user_id, company_id):
+    """DELETE the contact"""
+
+    # Retrieve the document from the database
+    company = Company.query.get_404(company_id)
+
+    if company is None:
+        # Return a 404 error if the company is not found
+        return jsonify({'error': 'company not found'}), 404
+
+    db.session.delete(company)
+    db.session.commit()
+
+    # Return the updated document data
+    return jsonify(message="Company deleted")
 
 # ------------------------
 # Set up view endpoints
