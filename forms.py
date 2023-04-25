e3b9740a615681a0e05d83dd509736863165fa67
Noushin Omidvar <noush.omidvar@gmail.com>
2023-04-24 23:21:19 -0600

    Add taskform

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField, SelectField, TextAreaField, BooleanField, DateField
 from wtforms.validators import DataRequired, Email, Length, URL
-import email_validator
+from datetime import date
+from models import Job
 
 
 class SignUpForm(FlaskForm):
@@ -41,3 +42,30 @@ class JobDetailForm(FlaskForm):
     post_url = StringField("Post URL", validators=[URL()])
     location = StringField("Location")
     notes = TextAreaField("Notes")
+
+
+class AddTaskForm(FlaskForm):
+    """Add new task form"""
+
+    def __init__(self, *args, **kwargs):
+        super(AddTaskForm, self).__init__(*args, **kwargs)
+        self.job.choices = [(job.id, job.job_title) for job in Job.query.all()]
+
+    def validate(self):
+        if not super().validate():
+            return False
+
+        # Set the job_id field based on the selected job
+        job_id = self.job.data
+        if job_id:
+            self.job_id.data = job_id
+
+        return True
+
+    task = StringField("Title", validators=[DataRequired()])
+    job = SelectField('Job', validators=[DataRequired()], render_kw={
+                      'placeholder': '+ Link to Job'})
+    startdate = DateField('Start Date', default=date.today)
+    enddate = DateField('End Date', default=date.today)
+    notes = TextAreaField("Notes")
+    completed = BooleanField("Mark as Completed")
