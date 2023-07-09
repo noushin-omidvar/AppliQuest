ab19fd3b7bbc4e46c09dfd66c8f73200c47cbf79
Noushin Omidvar <noush.omidvar@gmail.com>
2023-07-09 16:25:53 -0600

    update database

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+db.create_all()
+
 
 # ------------------------
 # Set up view endpoints
